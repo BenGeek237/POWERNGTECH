42ea0bd94376a021a26589b8ecaf271f253d02a4
BenGeek <bengeeks@icloud.com>
2026-09-07 15:05:19 +0200

    Fix: switch API baseURL from Render to new Namecheap VPS (api.powerngtech.com)

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 // Base Axios instance
 const api = axios.create({
-  baseURL: "https://powerngtech.onrender.com/api",
+  baseURL: "https://api.powerngtech.com/api",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
